bc7f146fa71cc2f66624725f93f471b39fdee66c
giglaulala <gigla.titina777@gmail.com>
2024-11-30 01:07:23 +0400

    feat: added search button logo

--- my-auto-2/src/app/content/Header/Header.jsx
+++ my-auto-2/src/app/content/Header/Header.jsx
@@ -58,7 +58,20 @@ export default function Header() {
           fill="#414042"
         ></path>
       </svg>
-
+      <button className="flex w-[32px] h-[32px] items-center justify-center bg-transparent border-0 cursor-pointer outline-none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+        >
+          <path
+            d="M13992.56 5132.11c1.27 0 2.466-.42 3.46-1.212l.478-.38.384-.476a5.487 5.487 0 0 0 1.236-3.486 5.563 5.563 0 0 0-5.558-5.555 5.563 5.563 0 0 0-5.559 5.555 5.563 5.563 0 0 0 5.559 5.554m7.614 2.89a.815.815 0 0 1-.58-.24l-2.326-2.299a7.527 7.527 0 0 1-4.708 1.65c-4.167 0-7.559-3.391-7.559-7.555 0-4.168 3.392-7.555 7.559-7.555s7.558 3.387 7.558 7.555a7.52 7.52 0 0 1-1.681 4.745l2.32 2.293a.823.823 0 0 1-.583 1.406z"
+            fill="#272a37"
+            transform="translate(-13976.999 -5111.001)"
+          ></path>
+        </svg>
+      </button>
       <button className="btn">Home</button>
       <button className="btn">Contact</button>
       <button className="btn">About</button>
